5587ab36a259b0d923bd3102dab982dc5d581c70
Johnathon Wood <jaywood128@gmail.com>
2018-10-19 20:18:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,3 +6,17 @@ def batch_badge_creator(array)
     badge_maker(name)
    end
 end
+def assign_rooms(array)
+  i = 0 
+  index = array[i]
+  attendees = names
+  while array[i] <= 7 
+    "Hello, #{name}! You'll be assigned to room #{index}"
+    i += 1
+  end
+end  
+    
+    
+    
+  
+  
