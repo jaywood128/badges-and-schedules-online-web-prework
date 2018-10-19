cb9ac82e124e915ab86286fc14eb1856aadce7b9
Johnathon Wood <jaywood128@gmail.com>
2018-10-19 20:40:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,13 +7,8 @@ def batch_badge_creator(array)
    end
 end
 def assign_rooms(array)
-  i = 0
-  index = array[i]
-  attendees = names
-  while array[i] <= 7
-    "Hello, #{name}! You'll be assigned to room #{index}"
-    i += 1
-  end
+  each_with_index(array) |name, i|
+  puts "Hello, #{name}! You'll be assigned to room #{array.index[i]}."
 end
 
 
