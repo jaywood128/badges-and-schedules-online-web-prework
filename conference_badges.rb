4820b452f2fec32559f438c8a3601feb1723d45d
Johnathon Wood <jaywood128@gmail.com>
2018-10-19 19:13:51 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,10 +2,6 @@ def badge_maker(name)
    "Hello, my name is #{name}."
 end
 def batch_badge_creator(array)
-  i = 0
-  array = badge_maker(name)
-  while i < array.length
-    array
-    i += 1
-  end
+  array.each do |name| 
+    badgemaker(name)
 end
